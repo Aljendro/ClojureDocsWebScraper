aaee895d72f45be2ff6506837c07dc0b5b6267d5
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2016-08-13 23:09:31 -0700

    Updated project.clj to include web scraping libraries

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject clojure-doc-web-scraper "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :description "Clojure docs web scraper to make flashcards with Anki"
+  :license {:name "GNU Affero General Public License"
+            :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [enlive "1.1.6"]
+                 [http-kit "2.1.18"]]
   :main ^:skip-aot clojure-doc-web-scraper.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
