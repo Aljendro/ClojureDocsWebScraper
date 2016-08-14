25bac5b222a6c37f02187aa91fb4f5bae83a4643
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2016-08-13 23:12:18 -0700

    Example usage of web scraper in core.-main

--- src/clojure_doc_web_scraper/core.clj
+++ src/clojure_doc_web_scraper/core.clj
@@ -1,7 +1,16 @@
 (ns clojure-doc-web-scraper.core
+  (:require [clojure-doc-web-scraper.scraper :as scraper] :reload
+            [clojure-doc-web-scraper.defaults :as defaults] :reload)
   (:gen-class))
 
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  "Runs the scraper"
+  []
+  (let [functions (-> (scraper/get-dom (str  defaults/clojure-docs-url "/clojure.core"))
+                       (scraper/find-functions)
+                       (scraper/extract-functions)
+                       (first)
+                       (list)
+                       (scraper/add-func-info))]
+    (first  functions)))
