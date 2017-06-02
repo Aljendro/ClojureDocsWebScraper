c009d97214ca7823f5094689aa0927df5f23d1dc
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2017-06-01 21:43:08 -0700

    Upgraded clojure version and added clojure.spec

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Clojure docs web scraper to make flashcards with Anki"
   :license {:name "GNU Affero General Public License"
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/spec.alpha "0.1.109"]
                  [enlive "1.1.6"]
                  [http-kit "2.1.18"]]
   :main ^:skip-aot clojure-doc-web-scraper.core
